deb23e0bbe0b417aadc27f6fb104cea6542bda81
Van156 <elapoamaya@gmail.com>
2022-11-18 23:04:19 -0500

    / de mas en baseUrl

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const baseURL = "http://localhost:5000";
-const baseURL = "https://thawing-earth-49122.herokuapp.com/";
+const baseURL = "https://thawing-earth-49122.herokuapp.com";
 
 const getToken = () => {
   return `Bearer ${localStorage.getItem("token")}`;
